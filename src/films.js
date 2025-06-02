12059411c363d0396a67a56878a90b6d843143d1
Albert Valls <avalls89@gmail.com>
2025-06-02 10:34:29 +0200

    Initial commit

--- src/films.js
+++ src/films.js
@@ -1,23 +1,23 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
-  return result;
+	let result =  ???;
+	console.log("EXERCICE 1 ->", result);
+	return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+
 }
 
-// Exercise 4:  Alphabetic order by title 
+// Exercise 4:  Alphabetic order by title
 function orderAlphabetically(array) {
-  
+
 }
 
 // Exercise 5: Order by year, ascending
@@ -37,7 +37,7 @@ function hoursToMinutes() {
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+
 }
 
 
@@ -45,14 +45,14 @@ function bestFilmOfYear() {
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = {
-    getAllDirectors,
-    getMoviesFromDirector,
-    moviesAverageOfDirector,
-    orderAlphabetically,
-    orderByYear,
-    moviesAverageByCategory,
-    hoursToMinutes,
-    bestFilmOfYear,
-  };
+	module.exports = {
+		getAllDirectors,
+		getMoviesFromDirector,
+		moviesAverageOfDirector,
+		orderAlphabetically,
+		orderByYear,
+		moviesAverageByCategory,
+		hoursToMinutes,
+		bestFilmOfYear,
+	};
 }
